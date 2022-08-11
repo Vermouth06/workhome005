e1e7164d0074104ebd35f0bf23e1d58443405d90
Daria <d.orlik000@gmail.com>
2022-08-11 05:14:29 +0300

    дз готово

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,41 @@ public class Main {
     public static void main(String[] args) {
         // 1
         int i = 0;
-        while
+        while (i < 10) {
+            i++;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (; i >= 1; i--) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
+
+        //2
+
+        int firstFridayOfMonth = 5;
+        for (int friday = firstFridayOfMonth; friday <= 31; friday += 7) {
+            System.out.printf("Сегодня пятница, %d-е число. Необходимо подготовить отчет.%n", friday);
+
+        }
+
+        //3
+        int period = 79;
+        int currentYear = 2022;
+        int start = currentYear - 200;
+        int end = currentYear + 100;
+        int startYear = 0;
+        for (int year = start; year <= end; year ++) {
+            if ( year % period == startYear) {
+                System.out.println(year);
+
+                //3
+
+            }
+        }
+
+
+
+        }
     }
-}
+
